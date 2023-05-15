7a70c37f506e79053c8e895b207ccd739f9efd6e
zmkhtr <azammukhtar3@gmail.com>
2023-05-15 13:09:27 +0700

    Initial Commit

---
+++ MVCtoMVVMtoMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVCtoMVVMtoMVP
+//
+//  Created by Azam Mukhtar on 15/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
