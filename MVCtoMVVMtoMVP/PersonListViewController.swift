a728cb98ed5f192d2175e0ac2c292a4615b04eb2
zmkhtr <azammukhtar3@gmail.com>
2023-05-16 11:12:26 +0700

    Rename ViewController to PersonListViewController

--- MVCtoMVVMtoMVP/PersonListViewController.swift
+++ MVCtoMVVMtoMVP/PersonListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PersonListViewController.swift
 //  MVCtoMVVMtoMVP
 //
 //  Created by Azam Mukhtar on 15/05/23.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PersonListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
