d485f64d189d394e481b16186e11f0cc7d5a7c27
zmkhtr <azammukhtar3@gmail.com>
2023-05-16 11:20:02 +0700

    Create struct Person

---
+++ MVCtoMVVMtoMVP/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  MVCtoMVVMtoMVP
+//
+//  Created by Azam Mukhtar on 16/05/23.
+//
+
+import UIKit
+
+struct Person {
+    let name: String
+    let age: Int
+    let image: UIImage
+}
