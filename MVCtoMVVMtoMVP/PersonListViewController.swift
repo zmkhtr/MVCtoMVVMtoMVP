285987b35162e19390ad9cb7040a99dfa0206113
zmkhtr <azammukhtar3@gmail.com>
2023-05-16 11:39:14 +0700

    Setup tableView with dummy data

--- MVCtoMVVMtoMVP/PersonListViewController.swift
+++ MVCtoMVVMtoMVP/PersonListViewController.swift
@@ -13,7 +13,27 @@ class PersonListViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "PersonCell")
+        var content = cell.defaultContentConfiguration()
+        
+        content.image = UIImage(systemName: "star.fill")
+        content.text = "Person name"
+        content.secondaryText = "Age \(12)"
+        
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
 }
 
