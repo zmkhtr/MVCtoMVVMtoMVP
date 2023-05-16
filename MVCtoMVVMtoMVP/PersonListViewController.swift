62d80841c57d441e55c74f9984c68005fdcb7bbc
zmkhtr <azammukhtar3@gmail.com>
2023-05-16 11:40:53 +0700

    Change hardcoded data to using data from persons List

--- MVCtoMVVMtoMVP/PersonListViewController.swift
+++ MVCtoMVVMtoMVP/PersonListViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class PersonListViewController: UITableViewController {
+    
+    private var persons: [Person] = [
+        Person(name: "Person 1", age: 23, image: UIImage(systemName: "person")!),
+        Person(name: "Person 2", age: 12, image: UIImage(systemName: "star")!),
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,16 +24,18 @@ class PersonListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return persons.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: "PersonCell")
         var content = cell.defaultContentConfiguration()
         
-        content.image = UIImage(systemName: "star.fill")
-        content.text = "Person name"
-        content.secondaryText = "Age \(12)"
+        let person = persons[indexPath.row]
+        content.image = person.image
+        content.text = person.name
+        content.secondaryText = "Age \(person.age)"
+        
         
         cell.contentConfiguration = content
         
