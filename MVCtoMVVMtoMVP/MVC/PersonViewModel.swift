c7e6dc163a10f48ac08ab6858262f5763d8f8c7c
zmkhtr <azammukhtar3@gmail.com>
2023-05-16 13:14:04 +0700

    Implement PersonViewModel load function and use the closure to capture the value changes

--- MVCtoMVVMtoMVP/MVC/PersonViewModel.swift
+++ MVCtoMVVMtoMVP/MVC/PersonViewModel.swift
@@ -10,5 +10,26 @@ import Foundation
 class PersonViewModel {
     
     private let loader = PersonLoader()
-
+    
+    var onLoadingStateChange: ((Bool) -> Void)?
+    var onErrorStateChange:((String?) -> Void)?
+    var onPersonsLoad: (([Person]) -> Void)?
+    
+    
+    func load() {
+        onLoadingStateChange?(true)
+        onErrorStateChange?(nil)
+        
+        loader.load { [weak self] result in
+            guard let self = self else { return }
+            self.onLoadingStateChange?(false)
+            
+            switch result {
+            case let .success(persons):
+                self.onPersonsLoad?(persons)
+            case .failure:
+                self.onErrorStateChange?("Error Fetching Data")
+            }
+        }
+    }
 }
